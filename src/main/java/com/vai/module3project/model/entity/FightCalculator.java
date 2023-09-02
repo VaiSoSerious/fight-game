c91324d68594cda6a9b690f888f4c026f03809df
VaiSoserious <antonywess@gmail.com>
2023-09-02 18:02:13 +0500

    Добавлена функция критического удара при расчете урона атакующим

--- src/main/java/com/vai/module3project/model/entity/FightCalculator.java
+++ src/main/java/com/vai/module3project/model/entity/FightCalculator.java
@@ -1,5 +1,10 @@
 package com.vai.module3project.model.entity;
 
+import lombok.extern.log4j.Log4j2;
+
+import java.util.Random;
+
+@Log4j2
 public class FightCalculator {
 
     private static String attackingCharacterHitText;
@@ -7,9 +12,15 @@ public class FightCalculator {
 
     public static void hit(Character attackingCharacter, Character defensiveCharacter) {
         int attackingCharacterDamage = attackingCharacter.getCharacterClass().getPower();
+        boolean crit = criticalHit();
+        String critText = crit?" критического":"";
+        if (crit) {
+            log.info(attackingCharacter + " наносит критический удар!");
+            attackingCharacterDamage *= 2;
+        }
         defensiveCharacterHealth = defensiveCharacter.getHealth() - attackingCharacterDamage;
         attackingCharacterHitText = attackingCharacter.getCharacterClass().getHit()
-                + " Нанеся: " + attackingCharacterDamage + " урона.";
+                + " Нанеся: " + attackingCharacterDamage + critText +  " урона.";
     }
 
     public static String getAttackingCharacterHitText() {
@@ -18,6 +29,12 @@ public class FightCalculator {
 
     public static int getDefensiveCharacterHealth() {
         return defensiveCharacterHealth;
+    }
+    private static boolean criticalHit() {
+        Random random = new Random();
+        int result = random.nextInt(2);
+//        return result > 0;
+        return result > 0? true:false;
     }
 }
 
